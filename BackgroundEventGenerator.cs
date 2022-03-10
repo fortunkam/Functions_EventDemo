041529bde5bef6c2d22c532b6371405f17528121
Matthew Fortunka <matthew@fortunka.co.uk>
2022-03-10 16:02:45 +0000

    Add project files.

---
+++ BackgroundEventGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HandleEvents
+{
+
+    public delegate void NewMessageEventHandler(string message);
+
+    public class BackgroundEventGenerator
+    {
+        private Thread _thread;
+
+        public void Start(string mailbox)
+        {
+            _thread = new Thread(new ParameterizedThreadStart(BackgroundThread));
+            _thread.Start(mailbox);
+        }
+
+        public void Stop()
+        {
+            _thread.Abort();
+        }
+
+        public event NewMessageEventHandler NewMessage;
+
+        private void BackgroundThread(object param)
+        {
+            var mb = param.ToString();
+
+            while (true)
+            {
+                if(NewMessage != null)
+                {
+                    NewMessage.Invoke(mb);
+                }
+
+                Thread.Sleep(5000);
+            }
+        }
+
+    }
+}
---
+++ Function1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace HandleEvents
+{
+    [StorageAccount("StorageConnectionString")]
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        [return: Queue("mailbox")]
+        public static string Run([QueueTrigger("mailbox")]string myQueueItem, TraceWriter log)
+        {
+            log.Info($"C# Queue trigger function processed: {myQueueItem}");
+            try
+            {
+                var eventGenerator = new BackgroundEventGenerator();
+                eventGenerator.NewMessage += (message) =>
+                {
+                    log.Info($"New Message arrived from mailbox {message}");
+                };
+
+                //var HubConnection = new SignalRHubConnection();
+                //HubConnection.On("Restart")(s =>
+                //{
+                //    eventGenerator.Stop();
+                //    return myQueueItem;
+                //});
+                //HubConnection.Start();
+
+                eventGenerator.Start(myQueueItem);
+
+                //Collect 1 minutes worth of events
+                Thread.Sleep(new TimeSpan(0, 1, 0));
+
+                eventGenerator.Stop();
+            }
+            catch(Exception ex)
+            {
+                log.Error(ex.ToString());
+                
+            }
+
+            return myQueueItem;
+        }
+    }
+}
